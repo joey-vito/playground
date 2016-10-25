fffae5839ae0752c6635a62a91e3e1730faabf82
Joey Johnson <joey.johnson7@icloud.com>
2016-10-25 10:26:05 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,99 @@
-console.log("playground is up and running");
 
-alert("playground isi up and running")
+
+// var x = 3;
+// var y = 4;
+
+// console.log(x + y);
+
+// alert("Hello" + " " + "World!");
+
+// var me = {firstName: "Joey", lastName: "Johnson", birthday: "10/26/1970"}
+
+// var interest = ["golfing", "family", "coding"]; 
+
+// console.log(me);
+// console.log(interest);
+
+// var z = 8;
+
+// if (z < 10) {
+// 	console.log("5 is less than 10");
+// } else {
+// 	console.log("5 is not less than 10");
+// }
+
+// function name(somePerson) {
+// 	return console.log(somePerson);
+// }
+
+// name("My name is Joey");
+
+
+
+// function print() {
+
+// }
+
+// console.log("Something")
+
+
+// function door(number) {
+// 	if(number == 1) {
+// 		alert("Congratulations!  You've won a car")
+// 	} else if(number == 2) {
+// 		alert("Congratulations!  You've won a cake")
+// 	} else {
+// 		alert("You've won absolutely nothing")
+// 	}
+// }
+
+// door(3);
+
+
+
+// var arr = ["Jake", "John", "Samantha", "Billy"];
+
+// for(var i = 0; i < arr.length; i++) {
+// 	console.log(arr[i] + " is my friend");
+// }
+
+// x = 99;
+
+// while(x >= 1) {
+// 	console.log(x + " bottles of beer on the wall. " + x + " bottles of beer...take one down, pass it around " + (x-1) + " bottles of beer on the wall")
+// 	x--;
+// }
+
+//create a variable that contains an array
+//create a for loop to run through the array in reverse order
+//add if/else statement to determine whether number is greater than or less than 10
+//if greater than, then alert "more is better"
+//if less than, then alert "less is more"
+
+
+// alert("Hello World!");
+
+// function debugme() {
+// 	var secret = "this is the secret";
+// 	console.log("before debugger");
+// 	debugger;
+// 	console.log("after debugger");
+// }
+
+// debugme();
+
+// 
+
+function loop() {
+	for(var i = 100; i >= 0; i--)
+		if(i % 7 == 0) {
+			console.log("lucky me");
+		} else {
+			console.log(i);
+		}
+}
+
+loop();
+
+	
+
